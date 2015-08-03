dd3581f4de56199d495b3aa1b3720995b6f3a0fd
nicholasvu <nicholas.t.vu@gmail.com>
2015-08-03 17:17:31 -0500

    Update CityBike.py

--- CityBike/CityBike.py
+++ CityBike/CityBike.py
@@ -1,3 +1,45 @@
+import time
+from dateutil.parser import parse
+import collections
+import sqlite3 as lite
+import requests
+from pandas.io.json import json_normalize
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# this script requires SQL Lite
+
+r = requests.get('http://www.citibikenyc.com/stations/json')
+df = json_normalize(r.json()['stationBeanList'])
+
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
+# create/populate reference data table
+with con:
+	cur.execute('CREATE TABLE citibike_reference (id INT PRIMARY KEY, totalDocks INT, city TEXT, altitude INT, stAddress2 TEXT, longitude NUMERIC, postalCode TEXT, testStation TEXT, stAddress1 TEXT, stationName TEXT, landMark TEXT, latitude NUMERIC, location TEXT )')
+
+sql = "INSERT INTO citibike_reference (id, totalDocks, city, altitude, stAddress2, longitude, postalCode, testStation, stAddress1, stationName, landMark, latitude, location) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+
+with con:
+	for station in r.json()['stationBeanList']:
+		cur.execute(sql,(station['id'],station['totalDocks'],station['city'],station['altitude'],station['stAddress2'],station['longitude'],station['postalCode'],station['testStation'],station['stAddress1'],station['stationName'],station['landMark'],station['latitude'],station['location']))		
+
+# create available bikes table
+station_ids = df['id'].tolist() 
+station_ids = ['_' + str(x) + ' INT' for x in station_ids] 
+
+with con:
+		cur.execute("CREATE TABLE available_bikes ( execution_time INT, " +  ", ".join(station_ids) + ");")
+
+# populate available bikes table with a hour of data
+for i in range(3):
+	r = requests.get('http://www.citibikenyc.com/stations/json')
+	exec_time = parse(r.json()['executionTime'])
+
+	cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
+	con.commit()
 
 	id_bikes = collections.defaultdict(int)
 	for station in r.json()['stationBeanList']:
